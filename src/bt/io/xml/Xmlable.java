ea80227d6ca7a594a041d0cbaed743dc21480be2
Bowtie8904 <30761922+Bowtie8904@users.noreply.github.com>
2020-06-20 11:06:42 +0200

    Initial commit.

---
+++ src/bt/io/xml/Xmlable.java
@@ -0,0 +1,18 @@
+package bt.io.xml;
+
+import org.dom4j.Document;
+
+/**
+ * Defines an Object that can be saved to an XML structure.
+ *
+ * @author &#8904
+ */
+public interface Xmlable
+{
+    /**
+     * Creates an XML Document that represents this instance.
+     *
+     * @return
+     */
+    public Document toXML();
+}
